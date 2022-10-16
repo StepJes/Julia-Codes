919f23fa1e90977ce4892683736be66effc4f8a2
Stephen Nadar <90613392+StepJes@users.noreply.github.com>
2022-10-16 23:22:37 +0530

    NEW-JULIA-CreationOfSet

---
+++ CreationOfSet.jl
@@ -0,0 +1,14 @@
+# Julia program to illustrate
+# the use of Sets
+
+# Creating an empty set
+Set1 = Set()
+println("Empty Set: ", Set1)
+
+# Creating a set with Integer values
+Set2 = Set([1, 2, 3, 4, 5, 2, 4, 6])
+println(Set2)
+
+# Creating a set with mixed datatypes
+Set3 = Set([1, 2, 3, "Hello", "Geeks"])
+println(Set3)
